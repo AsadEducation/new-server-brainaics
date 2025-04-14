cd45a6a52fbdf691e8c94ebca9b46f2670f0e6d5
sabila111 <sharminsharker2002@gmail.com>
2025-04-14 12:04:30 +0600

    added reward collection

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
     const columnCollection = database.collection("Columns");
     const taskCollection = database.collection("Tasks");
     const boardCollection = client.db("Brainiacs").collection("boards");
-
+    const rewardCollection = client.db("Brainiacs").collection("rewards");
 
 // my Profile reward section
 app.get("/myProfile", async (req, res) => {
